f4ccb56bef65cd5bd9b0eac0a6474ee0b33011be
DjGoodman <55036001+DjGoodman@users.noreply.github.com>
2024-10-30 17:43:27 +0000

    change to classic

--- resources/views/components/squashlevels-box.blade.php
+++ resources/views/components/squashlevels-box.blade.php
@@ -1,1 +1,1 @@
-<iframe src="https://app.squashlevels.com/boxes?clubid=5991" title="SquashLevels Box leagues" style="width:100%; height:100%;"></iframe>
+<iframe src="https://classic.squashlevels.com/boxes?clubid=5991" title="SquashLevels Box leagues" style="width:100%; height:100%;"></iframe>
